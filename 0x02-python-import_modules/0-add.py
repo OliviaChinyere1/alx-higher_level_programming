8b154be79129003ede13e40afa0d73956531c1aa
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-09-22 05:45:44 -0700

    fasting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 from add_0 import add
 
 if __name__ == '__main__':
-	a = 1
-	b = 2
-	c = add(a, b)
+    a = 1
+    b = 2
+    c = add(a, b)
 
-	print('{} + {} = {}'.format(a, b, c))
+    print('{} + {} = {}'.format(a, b, c))
