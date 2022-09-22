8df8aedda5135971a084c8677885e24afe1d6f96
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-09-21 17:06:38 -0700

    king14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for b in range(ord('z'), ord('a') - 1, -2):
-print("{:c}{:s}".format(b, chr(b - 33)), end="")
+for i in range(122, 96, -1):
+if (i % 2 != 0):
+print('{}'.format(chr(i - 32)), end='')
+else:
+ print('{}'.format(chr(i)), end='')
