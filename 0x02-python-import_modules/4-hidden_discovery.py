72786bea85c51d037b38b14fcd572b1a1cdec790
OliviaChinyere1 <unclechi2003@yahoo.com>
2022-09-22 06:33:37 -0700

    jumping om

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def principal():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
+
 if __name__ == "__main__":
-	import sys
-	import hidden_4
-	for n in dir(hidden_4):
-		if n[:2] != "__":
-		print(n)
+    principal()
